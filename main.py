a788d6eeddcc1921f20f8cc6195a246e3478999f
Tyr Mactire <tyr@pettingzoo.co>
2022-03-28 13:30:03 -0700

    write nginx file

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import os
 import requests
 
+from datetime import datetime
+
 def GetConfig():
     tor_url = 'https://check.torproject.org/torbulkexitlist'
     tor_url_env = os.environ.get('TOR_URL')
@@ -15,7 +17,6 @@ def GetConfig():
         file_path = file_path_env
 
     return tor_url, file_path
-
 
 def GetTorList(tor_url):
     r = requests.get(tor_url)
@@ -30,12 +31,21 @@ def GetTorList(tor_url):
 
     return ip_list
 
+def WriteNginxConf(file_path, ip_list):
+    today = datetime.today()
+    with open(file_path, "w") as block_file:
+        block_file.write("# MANAGED FILE: changes will be overwritten!\n")
+        block_file.write(f"# Tor block list. Generated: {today}\n\n")
+
+        for ip_address in ip_list:
+            block_file.write(f"deny {ip_address};\n")
+
+        block_file.write("\nallow all;\n")
+
 def main():
     tor_url, file_path = GetConfig()
-
     ip_list = GetTorList(tor_url)
-    for ip_address in ip_list:
-        print(f"ip: {ip_address}")
+    WriteNginxConf(file_path, ip_list)
 
 if __name__ == '__main__':
     main()
